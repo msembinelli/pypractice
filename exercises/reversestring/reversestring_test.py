915ab8ab9d6a283d38b03cdd185fe5e12406009e
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-04-01 19:04:44 -0600

    Update reversestring test

--- exercises/reversestring/reversestring_test.py
+++ exercises/reversestring/reversestring_test.py
@@ -4,24 +4,11 @@ from reversestring import reverse, reverse_join, reverse_manual, reverse_array
 class TestReverseStringMethods(unittest.TestCase):
 
     def test_isreversed(self):
-        self.assertEqual(reverse('apple'), 'elppa')
-        self.assertEqual(reverse('hello'), 'olleh')
-        self.assertEqual(reverse('Greetings!'), '!sgniteerG')
-
-    def test_isreversed_join(self):
-        self.assertEqual(reverse_join('apple'), 'elppa')
-        self.assertEqual(reverse_join('hello'), 'olleh')
-        self.assertEqual(reverse_join('Greetings!'), '!sgniteerG')
-
-    def test_isreversed_manual(self):
-        self.assertEqual(reverse_manual('apple'), 'elppa')
-        self.assertEqual(reverse_manual('hello'), 'olleh')
-        self.assertEqual(reverse_manual('Greetings!'), '!sgniteerG')
-
-    def test_isreversed_array(self):
-        self.assertEqual(reverse_array('apple'), 'elppa')
-        self.assertEqual(reverse_array('hello'), 'olleh')
-        self.assertEqual(reverse_array('Greetings!'), '!sgniteerG')
+        reverse_funcs = [reverse, reverse_array, reverse_manual, reverse_array]
+        for func in reverse_funcs:
+            self.assertEqual(func('apple'), 'elppa')
+            self.assertEqual(func('hello'), 'olleh')
+            self.assertEqual(func('Greetings!'), '!sgniteerG')
 
 if __name__ == '__main__':
     unittest.main()
